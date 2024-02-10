8efae22db6c781d05d95657d92237e05543b77b3
jennybsk <jekje9380@oslomet.no>
2024-02-10 21:16:15 +0100

    test commit

--- src/main/java/com/example/oblig1data1700/Oblig1Data1700Application.java
+++ src/main/java/com/example/oblig1data1700/Oblig1Data1700Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Oblig1Data1700Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Oblig1Data1700Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1Data1700Application.class, args);
+    }
 
 }
